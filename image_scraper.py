2af654b9151659914428f07f88eaa93ab76180c0
Takashi Mano <webtakashi@gmail.com>
2020-02-16 13:25:25 +0900

    s

--- image_scraper.py
+++ image_scraper.py
@@ -12,27 +12,48 @@ os.mkdir('image')
 os.mkdir('image/faces')
 
 
-class ImageDownloader(object):
+class GooglePager(object):
     def __init__(self, keyword):
-        session = requests.session()
-        session.headers.update(
+        self._keyword = keyword
+        self._session = requests.session()
+        self._session.headers.update(
             {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) \
              Gecko/20100101 Firefox/10.0'})
-        params = urllib.parse.urlencode(
-            {'q': keyword, 'tbm': 'isch', 'ijn': '1'})
-        query = "https://www.google.co.jp/search" + '?' + params
-        self._bs = BeautifulSoup(session.get(query).text, 'html.parser')
+        self._page = 1
+
+    def __iter__(self):
+        while True:
+            print(self._page)
+            params = urllib.parse.urlencode(
+                {'q': self._keyword, 'tbm': 'isch', 'ijn': '10'})
+            query = "https://www.google.co.jp/search" + '?' + params
+            self._page += 1
+            if self._page <= 10:
+                yield self._session.get(query)
+            else:
+                self._page = 1
+                return
+
+
+class ImageDownloader(object):
+
+    def __init__(self, keyword):
+        self._keyword = keyword
+        pass
+        # self._bs = BeautifulSoup(session.get(query).text, 'html.parser')
 
     def go(self):
         downloads = []
-        for img in self._bs.find_all('img'):
-            try:
-                url = img['data-iurl']
-                downloads.append('image/image_{}.jpg'.format(len(downloads) + 1))
-                urllib.request.urlretrieve(url, downloads[-1])
-            except KeyError:
-                print('failed to get url : {}'.format(img))
-        return downloads
+        for page in GooglePager(self._keyword):
+            bs = BeautifulSoup(page.text, 'html.parser')
+            for img in bs.find_all('img'):
+                try:
+                    url = img['data-iurl']
+                except KeyError:
+                    print('failed to get url : {}'.format(img))
+                else:
+                    downloads.append('image/image_{}.jpg'.format(len(downloads) + 1))
+                    urllib.request.urlretrieve(url, downloads[-1])
 
 
 class FaceDetector(object):
@@ -56,6 +77,16 @@ class FaceDetector(object):
 
 if __name__ == '__main__':
 
+    ImageDownloader('アンジェ・カトリーナ').go()
+
+    '''
     downloads = ImageDownloader('アンジェ・カトリーナ').go()
     for i, d in enumerate(downloads):
         FaceDetector(d).cutout_faces('image/faces/faces_{}.jpg'.format(i + 1))
+    '''
+
+    '''
+    for page in GooglePager('アンジェ・カトリーナ'):
+        print(page.text)
+    '''
+
